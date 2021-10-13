7bcac45ce90400be5057730c1d61d3d0da1fa399
krishnasandhar <92383385+krishnasandhar@users.noreply.github.com>
2021-10-13 10:51:21 +0530

    Add files via upload

---
+++ CateogoryModal.kt
@@ -0,0 +1,12 @@
+package dev.company.jetshop.ui.cateogory
+
+class CateogoryModal {
+
+    var iconschar:Int?=0
+    var alphaChar:String?=null
+
+    constructor (iconschar: Int?, alphaChar: String?) {
+        this.iconschar = iconschar
+        this.alphaChar = alphaChar
+    }
+}
